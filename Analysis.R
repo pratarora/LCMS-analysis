ff0df027cf28ea08f936031357abe15b6ce0125a
pratarora <pratarora@gmail.com>
2019-01-18 00:20:18 +0530

    cleaned code report progress

--- Analysis.R
+++ Analysis.R
@@ -2,7 +2,6 @@ rm(list=ls())
 setwd("C:/Users/prata/Desktop/Elucidata/LCMS analysis/LCMS-analysis/")
 getwd()
 metadata <- read.csv("sample_metadata.csv", header = TRUE)
-compound_rt_database <- read.csv("compound_rt_database.csv", header = TRUE)
 maven_data <- read.csv("Maven_processed.csv", header=TRUE)
 
 library(dplyr)
@@ -14,18 +13,48 @@ library(reshape2)
 
 pool_total <- maven_data %>% select(-c(label,metaGroupId,groupId,goodPeakCount,
                                        medMz,medRt,maxQuality,expectedRtDiff,
-                                       ppmDiff,parent,note)) %>%
-  group_by(compound,formula,compoundId) %>% summarize_all(funs(sum))
+                                       ppmDiff,parent,note,formula,compoundId)) %>%
+  group_by(compound) %>% summarize_all(funs(sum))
 
-# PCA non raw data
-for (i in 4:ncol(pool_total)) {
+# Correction for R reading of numerical as factors
+metadata$Sample <- paste0("X",metadata$Sample)
+
+df2 <- maven_data %>% select(-c(label,metaGroupId,groupId,goodPeakCount,
+                                medMz,medRt,maxQuality,expectedRtDiff,
+                                ppmDiff,parent, compoundId, formula)) %>% 
+  group_by(compound,note)
+
+df3 <- melt(df2,id=c("compound","note"))
+
+names(df3)[names(df3)=="variable"] <- "Sample"
+
+df4 <- full_join(df3,metadata)
+
+df5 <- pool_total %>% ungroup() %>% 
+  group_by(compound)
+df6 <- melt(df5,id=c("compound"))
+names(df6)[names(df6)=="variable"] <- "Sample"
+names(df6)[names(df6)=="value"] <- "Total"
+df7 <- full_join(df6,metadata)
+
+
+fraction_enrichment <- full_join(df4,df7)
+fraction_enrichment <- fraction_enrichment %>% mutate("fraction_enrich"=(value/Total)*100)
+write.csv(fraction_enrichment,
+          file="Data with Pool and fractions.csv")
+
+
+
+
+# PCA non transformed raw data
+for (i in 2:ncol(pool_total)) {
   p <- ggplot(pool_total,
               aes(x=unlist(pool_total[,i],use.names=FALSE)))+
     geom_histogram(bins=10)
   print(p)
 }
 
-pca <- prcomp(pool_total[,c(4:19)])
+pca <- prcomp(pool_total[,c(2:ncol(pool_total))])
 summary(pca)
 ggbiplot(pca)
 
@@ -33,19 +62,19 @@ ggbiplot(pca)
 
 # PCA normalized data
 pool_total_normalized <- pool_total
-for (i in 4:ncol(pool_total)) {
+for (i in 2:ncol(pool_total)) {
   pool_total_normalized[,i] <- scale(pool_total[,i],center = TRUE) %>% as.vector
   
 }
 
 
-pca.normalized <- prcomp(pool_total_normalized[,c(4:19)])
+pca.normalized <- prcomp(pool_total_normalized[,c(2:ncol(pool_total))])
 summary(pca.normalized)
 ggbiplot(pca.normalized)
 
 # PCA log transformed data
 pool_total_log <- pool_total
-for (i in 4:ncol(pool_total)) {
+for (i in 2:ncol(pool_total)) {
   pool_total_log[,i] <- log10(pool_total[,i]+1) %>% as.vector
   
 }
@@ -57,11 +86,11 @@ for (i in 4:ncol(pool_total)) {
 }
 
 pca.labels <- pool_total_log$compound
-pca.log <- prcomp(pool_total_log[,c(4:19)])
+pca.log <- prcomp(pool_total_log[,c(2:ncol(pool_total))])
 
 pca.log$sdev
 screeplot(pca, type="lines",col=3)
-pca$rotation # with princomp(): pca$loadings
+pca$rotation
 summary(pca.log)
 
 ggbiplot(pca.log, ellipse=TRUE, labels=pca.labels)
@@ -84,31 +113,6 @@ dev.copy(pdf,
          height= 8,
          width=12)
 dev.off()
-metadata$Sample <- paste0("X",metadata$Sample)
-
-df2 <- maven_data %>% select(-c(label,metaGroupId,groupId,goodPeakCount,
-                                medMz,medRt,maxQuality,expectedRtDiff,
-                                ppmDiff,parent, compoundId, formula)) %>% 
-  group_by(compound,note)
-
-df3 <- melt(df2,id=c("compound","note"))
-
-names(df3)[names(df3)=="variable"] <- "Sample"
-
-df4 <- full_join(df3,metadata)
-
-df5 <- pool_total %>% ungroup()%>% select(-c(compoundId, formula)) %>% 
-  group_by(compound)
-df6 <- melt(df5,id=c("compound"))
-names(df6)[names(df6)=="variable"] <- "Sample"
-names(df6)[names(df6)=="value"] <- "Total"
-df7 <- full_join(df6,metadata)
-
-
-fraction_enrichment <- full_join(df4,df7)
-fraction_enrichment <- fraction_enrichment %>% mutate("fraction_enrich"=(value/Total)*100)
-write.csv(fraction_enrichment,
-          file="Data with Pool and fractions.csv")
 
 totalgraph <- ggplot(fraction_enrichment,
                      aes(x=Time..mins.,
